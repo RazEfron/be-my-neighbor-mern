62250b69b7590b0129ba8c1697b239fedf9333db
Raz <razefron01@gmail.com>
2020-06-29 18:08:44 +0300

    MongoDB connected

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MongoURI: `mongodb+srv://razefron:${process.env.CONNECT_PASSWORD}@bemyneighbor.glpyy.mongodb.net/<dbname>?retryWrites=true&w=majority`,
+};
--- server.js
+++ server.js
@@ -11,12 +11,34 @@ const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
 // Certifaicate Setup
-
 const key = fs.readFileSync('./config/certkey/privateKey.key');
 const cert = fs.readFileSync('./config/certkey/certificate.crt');
 
 //  Load env file
-
 dotenv.config({path: './config.env'});
 
-// 
+//  Load Database Connection
+const db = require("./config/keys").MongoURI;
+
+// Connect Port to mongo DB online
+mongoose.connect(db, {
+    useFindAndModify: false, 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB is connected')).catch(err => console.log(err))
+
+//  Server Ports
+const PORT = process.env.PORT || 9000;
+
+let Server
+if (process.env.NODE_ENV === 'development') {
+    Server = https.createServer({key: key, cert: cert}, app);
+} else {
+    Server = app
+}
+
+Server.listen(PORT, () => {
+    console.log(
+        `Server is running at ${process.env.NODE_ENV} made on port ${PORT}`
+    )
+})
